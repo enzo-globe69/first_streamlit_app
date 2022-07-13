fffecfdc8a5ef775be1d6c9713d53877653dcbb5
enzo-globe69 <109204197+enzo-globe69@users.noreply.github.com>
2022-07-13 15:21:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Aligue Sinangag with Tinapa Flakes')
 streamlit.text('🥗 Kamote, Kangkong, at Boba Milk Tea')
 streamlit.text('🥚 Nilagang itlog galing sa lakwatserong manok')
 streamlit.text('🥑🍞 Tustadong Avocado')
+
+streamlit.header('🍌🥭 Gawa kayo ng sariling niyonh Fruit Smoothie 🥝🍇')
