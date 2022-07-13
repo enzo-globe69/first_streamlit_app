8c7f37b6e2d5996e7a952084bebee1d7d454d75d
enzo-globe69 <109204197+enzo-globe69@users.noreply.github.com>
2022-07-13 15:17:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Almusal Menu')
+streamlit.text('Aligue Sinangag with Tinapa Flakes')
+streamlit.text('Kamote, Kangkong, at Boba Milk Tea')
+streamlit.text('Nilagang itlog galing sa lakwatserong manok')
