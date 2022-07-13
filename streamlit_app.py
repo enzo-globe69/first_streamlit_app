7d6f630b3024a948979959f5bd7cdb38de095de5
enzo-globe69 <109204197+enzo-globe69@users.noreply.github.com>
2022-07-13 15:23:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🥚 Nilagang itlog galing sa lakwatserong manok')
 streamlit.text('🥑🍞 Tustadong Avocado')
 
 streamlit.header('🍌🥭 Gawa kayo ng sariling niyonh Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
